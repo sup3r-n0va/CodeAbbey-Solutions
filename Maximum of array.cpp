bc69342f03c7cb9b71a00b53a7266f808526a6ec
sup3r-n0va <Sup3rn0va@protonmail.com>
2016-11-02 01:25:17 +0000

    Create Maximum of array.cpp

---
+++ Maximum of array.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	const int ARRAY_SIZE = 299;
+	int values[ARRAY_SIZE];
+	int loop;
+	int max;
+	int min;
+	int loop2;
+	
+	
+	max = values[0];
+	
+    for ( loop = 1; loop <= ARRAY_SIZE; loop++) {
+	    cin >> values[loop];
+		if( values[loop] >= max) {
+			max = values[loop];
+		}
+		
+	}
+	printf("%d\n", max);
+  printf("    ");	
+	min = values[1];
+	
+	for (loop2 = 2; loop2 <= ARRAY_SIZE; loop2++) {
+	    cin >> values[loop2];
+	    if( values[loop2] <= min) {
+	        min = values[loop2];
+	    }
+	}
+    printf("%d\n", min);
+    
+	return 0;
+}
